320ae1bc14873644a485c3013445bb2fc012ac4e
jasonmwatu <jasonmwatu@gmail.com>
2022-09-13 00:20:25 +0300

    add more content to about component. style about component on small screen sizes from 75vh to 100vh

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,5 @@
 import "./about.css";
-import about from "../../img/about.jpg"
+import about from "../../img/about.jpg";
 
 const About = () => {
   return (
@@ -7,11 +7,7 @@ const About = () => {
       <div className="a-left">
         <div className="a-card bg"></div>
         <div className="a-card">
-          <img
-            src={about}
-            alt=""
-            className="a-img"
-          />
+          <img src={about} alt="" className="a-img" />
         </div>
       </div>
       <div className="a-right">
@@ -25,6 +21,24 @@ const About = () => {
           hard-work and I am persistent in the quest for personal and group
           excellence. I am always willing to try different ideas and ways of
           doing things when the need arises.
+        </p>
+        <br />
+        <p className="a-desc">
+          I have extensive training and experience as a web developer, designer
+          and UI/UX expert. My skills are based on my own career path which has
+          taken me from developing simple calculator games to a fully-fledged
+          full-stack web apps. I specialize in website development, mobile app
+          development and user interface design for online stores. My
+          professional services range from pre-launch to production to
+          maintenance.
+        </p>
+        <br />
+        <p className="a-desc">
+          I will help make your product a success by creating engaging websites
+          and great-looking mobile apps that convert visitors into loyal
+          customers. I guarantee to deliver websites with an intuitive design,
+          easy navigation and a solid structure to meet the needs of both
+          private and corporate businesses.
         </p>
       </div>
     </div>
