a9a225df486ee747fab028b6c68c4561e849b8fa
jasonmwatu <jasonmwatu@gmail.com>
2022-09-13 05:51:10 +0300

    add full screen menu on toggle component and it's styling

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import "./toggle.css";
 import Sun from "../../img/sun.png";
 import Moon from "../../img/moon.png";
+// import HamburgerMenu from "./Hamburger";
 // import { useContext } from "react";
 // import { ThemeContext } from "../../context";
 
@@ -9,12 +12,14 @@ import { uiActions } from "../../store/index";
 
 const Toggle = () => {
   // const theme = useContext(ThemeContext);
+  const [menuOpen, setMenuOpen] = useState(false);
   const dispatch = useDispatch();
   const darkMode = useSelector((state) => state.ui.darkMode);
 
   const toggleDarkMode = () => {
     dispatch(uiActions.toggle());
   };
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   // const handleClick = () => {
   //   // theme.dispatch({ type: "TOGGLE" });
@@ -22,13 +27,33 @@ const Toggle = () => {
   // };
   return (
     <>
-      <div className="m" style={{ boxShadow: darkMode && "0 0 1rem 0 rgba(243, 243, 243, 0.2)", backgroundColor: "rgba(194, 194, 194, 0.26)"}}>
+      {/* <HamburgerMenu /> */}
+      <div
+        className="m"
+        onClick={toggleMenu}
+        style={{
+          boxShadow: darkMode && "0 0 1rem 0 rgba(243, 243, 243, 0.2)",
+          backgroundColor: "rgba(194, 194, 194, 0.26)",
+        }}
+      >
         {/* <IoMenu style={{color: darkMode && "white", height: "4em"}}/> */}
-        <span className="m-line" style={{backgroundColor: darkMode && "white"}}></span>
-        <span className="m-line" style={{backgroundColor: darkMode && "white"}}></span>
-        <span className="m-line" style={{backgroundColor: darkMode && "white"}}></span>
+        <span
+          className="m-line"
+          style={{ backgroundColor: darkMode && "white", transform: menuOpen && "rotate(-135deg) translate(-3px, -3px)" }}
+        ></span>
+        <span
+          className="m-line"
+          style={{ backgroundColor: darkMode && "white", display: menuOpen && "none" }}
+        ></span>
+        <span
+          className="m-line"
+          style={{ backgroundColor: darkMode && "white", transform: menuOpen && "rotate(135deg) translate(-3px, 3px)" }}
+        ></span>
       </div>
-      <div className="t"style={{ boxShadow: darkMode && "0 0 1rem 0 rgba(243, 243, 243, 0.2)"}}>
+      <div
+        className="t"
+        style={{ boxShadow: darkMode && "0 0 1rem 0 rgba(243, 243, 243, 0.2)" }}
+      >
         <img src={Sun} alt="light mode toggle" className="t-icon" />
         <img src={Moon} alt="dark mode toggle" className="t-icon" />
         <div
@@ -37,6 +62,31 @@ const Toggle = () => {
           style={{ left: darkMode ? 0 : 25 }}
         ></div>
       </div>
+      {/* navigation main body starts*/}
+      <nav
+        className="m-nav"
+        onClick={toggleMenu}
+        style={{
+          width: menuOpen ? "100%" : "0",
+          opacity: menuOpen ? "1" : "0",
+        }}
+      >
+        <ul className="m-list">
+          <li className="m-list-item" style={{ color: darkMode && "white" }}>
+            <AnchorLink href="#">Home</AnchorLink>
+          </li>
+          <li className="m-list-item" style={{ color: darkMode && "white" }}>
+          <AnchorLink href="#">About</AnchorLink>
+          </li>
+          <li className="m-list-item" style={{ color: darkMode && "white" }}>
+          <AnchorLink href="#">Portfolio</AnchorLink>
+          </li>
+          <li className="m-list-item" style={{ color: darkMode && "white" }}>
+          <AnchorLink href="#">Contact Me</AnchorLink>
+          </li>
+        </ul>
+      </nav>
+      {/* navigation main body ends */}
     </>
   );
 };
